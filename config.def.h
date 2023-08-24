3ea84e81b66051db78e7579441c3d7fddad97d1f
alpheratz0 <alpheratz99@protonmail.com>
2023-08-24 19:48:11 -0300

    change border size to 15px

--- config.def.h
+++ config.def.h
@@ -7,7 +7,7 @@
  */
 
 static char *font = "Terminus:pixelsize=15:antialias=true:autohint=true";
-static int borderpx = 2;
+static int borderpx = 15;
 
 /*
  * What program is execed by st depends of these precedence rules:
